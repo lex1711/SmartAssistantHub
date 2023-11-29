3d0612a95f06d9eb7ac794c9f8cd78679d42dd78
Oleksii <al00xey@gmail.com>
2023-11-29 20:54:17 +0100

    TaskMaster Techniques Service - SmartTask Strategy Service
    Erste API

---
+++ Mango.Services.StrategyApi/Models/Strategy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Mango.Services.StrategyApi.Models
+{
+	public class Strategy
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<string>? Categories { get; set; } // Список категорий
+        public string? ComplexityLevel { get; set; } // Уровень сложности
+        public string? RecommendedFor { get; set; } // Рекомендуемые типы задач
+        public List<string>? Resources { get; set; } // Ссылки на ресурсы (статьи, видео)
+        public bool IsAIEnhanced { get; set; } // Поддержка усиления через ИИ
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public List<string>? Tags { get; set; } // Теги для поиска и фильтрации
+
+        
+        public enum TechniqueCategory
+           {       
+        TimeManagement,
+        Productivity,
+        GoalSetting,
+        StressManagement,
+        Others
+        // Другие категории по необходимости
+        }
+    }
+}
+
