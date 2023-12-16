6fcd5bbbfbbd03af71f031bb7346014864e33cd7
Oleksii <al00xey@gmail.com>
2023-12-16 21:39:40 +0100

    Task<ResponseDto?> SendAsync

--- Mango.Services.StrategyApi/Models/Dto/StrategyDto.cs
+++ Mango.Services.StrategyApi/Models/Dto/StrategyDto.cs
@@ -19,4 +19,3 @@ namespace Mango.Services.StrategyApi.Models.Dto
 
     }
 }
-
--- Mango.Web/Models/ResponseDto.cs
+++ Mango.Web/Models/ResponseDto.cs
@@ -5,19 +5,16 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
-namespace Mango.Services.StrategyApi.Models.Dto
+namespace Mango.Web.Models
 {
     // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
-    [HtmlTargetElement("tag-name")]
-    public class ResponseDto : TagHelper
+    //[HtmlTargetElement("tag-name")]
+    public class ResponseDto //: TagHelper
     {
         public object? Resault { get; set; }
         public bool IsSuccess { get; set; } = true;
         public string Message { get; set; } = "";
 
-        public override void Process(TagHelperContext context, TagHelperOutput output)
-        {
-
-        }
+        //public override void Process(TagHelperContext context, TagHelperOutput output) {}
     }
 }
