f0a327ff9fbed3d3d603cb2c3a1f7f55be695f3c
Oleksii <al00xey@gmail.com>
2023-12-17 18:14:22 +0100

    Create BaseService.cs

---
+++ Mango.Web/Service/BaseService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Mango.Web.Models;
+using Mango.Web.Service.IService;
+
+namespace Mango.Web.Service
+{
+	public class BaseService: IBaseService
+	{
+        private readonly IHttpClientFactory _httpClientFactory;
+		public BaseService(IHttpClientFactory httpClientFactory)
+		{
+            _httpClientFactory = httpClientFactory;
+
+		}
+
+        public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+        {
+            // throw new NotImplementedException();
+        }
+    }
+}
+
