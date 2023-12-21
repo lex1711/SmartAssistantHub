9ec970273fdba49f830224f721b5b53976d2dde1
Oleksii <al00xey@gmail.com>
2023-12-21 22:20:28 +0100

    CRUD

--- Mango.Web/Service/StrategyService.cs
+++ Mango.Web/Service/StrategyService.cs
@@ -5,54 +5,79 @@ using Mango.Web.Utility;
 
 namespace Mango.Web.Service
 {
-	public class StrategyService:IStrategyService
-	{
-		private readonly IBaseService _baseService;
+    public class StrategyService : IStrategyService
+    {
+        private readonly IBaseService _baseService;
 
 
-		public StrategyService(IBaseService baseService)
-		{
-			_baseService = baseService;
-		}
+        public StrategyService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
 
         public async Task<ResponseDto?> CreateStrategysAsync(StrategyDto strategyDto)
         {
-            throw new NotImplementedException();
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = strategyDto,
+                Url = SD.StrategyAPIBase + "/api/strategy"
+            });
         }
 
         public async Task<ResponseDto?> DeleteStrategysAsync(int id)
         {
-            throw new NotImplementedException();
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.DELETE,
+                Url = SD.StrategyAPIBase + "/api/strategy/" + id
+            });
         }
 
         public async Task<ResponseDto?> GetAllStrategysAsync()
         {
             return await _baseService.SendAsync(new RequestDto()
             {
-                ApiType=SD.ApiType.GET,
-                Url = SD.StrategyAPIBase+"/api/strategy"
+                ApiType = SD.ApiType.GET,
+                Url = SD.StrategyAPIBase + "/api/strategy"
             });
         }
 
         public async Task<ResponseDto?> GetStrategyAsync(string strategyCode)
         {
-            throw new NotImplementedException();
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.StrategyAPIBase + "/api/strategy/GetByName"
+            });
         }
 
         public async Task<ResponseDto?> GetStrategyByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.StrategyAPIBase + "/api/strategy/" + id
+            });
         }
 
         public async Task<ResponseDto?> GetStrategyByNameAsync(string strategyName)
         {
-            throw new NotImplementedException();
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.StrategyAPIBase + "/api/strategy/GetByName" + strategyName
+            });
         }
 
-        public Task<ResponseDto?> UpdateStrategysAsync(StrategyDto strategyDto)
+        public async Task<ResponseDto?> UpdateStrategysAsync(StrategyDto strategyDto)
         {
-            throw new NotImplementedException();
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.PUT,
+                Data = strategyDto,
+                Url = SD.StrategyAPIBase + "/api/strategy"
+            });
         }
     }
 }
-
